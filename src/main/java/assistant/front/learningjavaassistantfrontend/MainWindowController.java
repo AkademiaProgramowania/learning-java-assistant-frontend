b69070f20a17553b62730d3c3df19b61b769674c
Aleksy Greiveldinger <kapitanaleksy@gmail.com>
2023-03-17 12:30:09 +0100

    add an empty main window which will be developed in the future

---
+++ src/main/java/assistant/front/learningjavaassistantfrontend/MainWindowController.java
@@ -0,0 +1,12 @@
+package assistant.front.learningjavaassistantfrontend;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainWindowController {
+    private Service service = new Service();
+
+    @FXML
+    private Label title;
+
+}
