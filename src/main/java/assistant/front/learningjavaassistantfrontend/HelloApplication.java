311db9accd92ac39d32dc7097612662d08a2f76c
Aleksy Greiveldinger <kapitanaleksy@gmail.com>
2023-03-31 13:21:42 +0200

    refactor ViewFactory (get rid of repetition)

--- src/main/java/assistant/front/learningjavaassistantfrontend/HelloApplication.java
+++ src/main/java/assistant/front/learningjavaassistantfrontend/HelloApplication.java
@@ -1,21 +1,12 @@
 package assistant.front.learningjavaassistantfrontend;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import static assistant.front.learningjavaassistantfrontend.GUIParameters.REGISTER_HEIGHT;
-import static assistant.front.learningjavaassistantfrontend.GUIParameters.REGISTER_WIDTH;
+import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
-        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-window.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), REGISTER_WIDTH, REGISTER_HEIGHT);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();*/
         ViewFactory viewFactory = new ViewFactory();
         viewFactory.showRegisterWindow();
     }
--- src/main/java/assistant/front/learningjavaassistantfrontend/ViewFactory.java
+++ src/main/java/assistant/front/learningjavaassistantfrontend/ViewFactory.java
@@ -8,32 +8,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ViewFactory {
+
     public void showRegisterWindow() {
         Controller controller = new RegisterWindowController(this);
+        showWindow(controller);
+    }
+
+    public void showMainWindow() {
+        Controller controller = new MainWindowController(this);
+        showWindow(controller);
+    }
+
+    private void showWindow(Controller controller) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFXMLName()));
         fxmlLoader.setController(controller);
         Parent parent;
         try {
             parent = fxmlLoader.load();
         } catch(IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        Scene scene = new Scene(parent);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void showMainWindow() {
-        Controller controller = new MainWindowController(this);
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFXMLName()));
-        fxmlLoader.setController(controller);
-        Parent parent;
-        try {
-            parent = fxmlLoader.load();
-        } catch (IOException e) {
             e.printStackTrace();
             return;
         }
