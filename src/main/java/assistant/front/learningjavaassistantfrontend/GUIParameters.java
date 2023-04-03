0c4bc3bf44e405481300f5832dbf321ea1560fe2
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-04-03 19:54:22 +0200

    //

--- src/main/java/assistant/front/learningjavaassistantfrontend/GUIParameters.java
+++ src/main/java/assistant/front/learningjavaassistantfrontend/GUIParameters.java
@@ -3,4 +3,6 @@ package assistant.front.learningjavaassistantfrontend;
 public class GUIParameters {
     public static final int REGISTER_WIDTH = 515;
     public static final int REGISTER_HEIGHT = 303;
+    public static final int MAIN_WIDTH = 1112;
+    public static final int MAIN_HEIGHT = 872;
 }
--- src/main/java/assistant/front/learningjavaassistantfrontend/HelloApplication.java
+++ src/main/java/assistant/front/learningjavaassistantfrontend/HelloApplication.java
@@ -7,14 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import static assistant.front.learningjavaassistantfrontend.GUIParameters.REGISTER_HEIGHT;
-import static assistant.front.learningjavaassistantfrontend.GUIParameters.REGISTER_WIDTH;
+import static assistant.front.learningjavaassistantfrontend.GUIParameters.*;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("register-window.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), REGISTER_WIDTH, REGISTER_HEIGHT);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), MAIN_WIDTH, MAIN_HEIGHT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
